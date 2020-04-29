985955e03338aafbad4ee4adca07bd1bb7da0515
Trefluuu <miulewski@gmail.com>
2020-04-29 20:44:14 +0200

    lab06

---
+++ PW/lab06/BackgroundWorkerSample/BackgroundWorkerSample/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Threading;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BackgroundWorkerSample
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// The backgroundworker object on which the time consuming operation shall be executed
+        /// </summary>
+        BackgroundWorker m_oWorker;
+
+        public Form1()
+        {
+            InitializeComponent();
+            m_oWorker = new BackgroundWorker();
+            m_oWorker.DoWork += new DoWorkEventHandler(m_oWorker_DoWork);
+            m_oWorker.ProgressChanged += new ProgressChangedEventHandler(m_oWorker_ProgressChanged);
+            m_oWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(m_oWorker_RunWorkerCompleted);
+            m_oWorker.WorkerReportsProgress = true;
+            m_oWorker.WorkerSupportsCancellation = true;
+        }
+
+        /// <summary>
+        /// On completed do the appropriate task
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void m_oWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //If it was cancelled midway
+            if (e.Cancelled)
+            {
+                lblStatus.Text = "Task Cancelled.";
+            }
+            else if (e.Error != null)
+            {
+                lblStatus.Text = "Error while performing background operation.";
+            }
+            else
+            {
+                lblStatus.Text = "Task Completed...";
+            }
+            btnStartAsyncOperation.Enabled = true;
+            btnCancel.Enabled = false;
+        }
+
+        /// <summary>
+        /// Notification is performed here to the progress bar
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void m_oWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //Here you play with the main UI thread
+            progressBar1.Value = e.ProgressPercentage;
+            lblStatus.Text = "Processing......" + progressBar1.Value.ToString() + "%";
+        }
+
+        /// <summary>
+        /// Time consuming operations go here </br>
+        /// i.e. Database operations,Reporting
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void m_oWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //NOTE : Never play with the UI thread here...
+            int n;
+            //time consuming operation
+            int lp = 0;
+            Random rand = new Random();
+            while (true)
+            {
+                Thread.Sleep(100);
+                n = rand.Next(1, 1000);
+                if (czy_pierwsza(n))
+                {
+                    lp++;
+                    if (lp == 100)
+                    {
+                        m_oWorker.ReportProgress(100);
+                        return;
+                    }
+                    m_oWorker.ReportProgress(lp);
+                }
+
+                //If cancel button was pressed while the execution is in progress
+                //Change the state from cancellation ---> cancel'ed
+                if (m_oWorker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    m_oWorker.ReportProgress(0);
+                    return;
+                }
+            }
+            
+            
+            //Report 100% completion on operation completed
+        }
+
+        private void btnStartAsyncOperation_Click(object sender, EventArgs e)
+        {
+            btnStartAsyncOperation.Enabled  = false;
+            btnCancel.Enabled               = true;
+            //Start the async operation here
+            m_oWorker.RunWorkerAsync();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if (m_oWorker.IsBusy)
+            {
+                //Stop/Cancel the async operation here
+                m_oWorker.CancelAsync();
+            }
+        }
+        bool czy_pierwsza(int n)
+        {
+            if (n < 2)
+                return false; //gdy liczba jest mniejsza niż 2 to nie jest pierwszą
+
+            for (int i = 2; i * i <= n; i++)
+                if (n % i == 0)
+                    return false; //gdy znajdziemy dzielnik, to dana liczba nie jest pierwsza
+            return true;
+        }
+    }
+}
