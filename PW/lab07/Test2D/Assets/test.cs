03f4c158eee5be7483d51c3993bc06c8b547ac41
Trefluuu <miulewski@gmail.com>
2020-05-07 21:43:16 +0200

    lab07

---
+++ PW/lab07/Test2D/Assets/test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public int x = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i=0;i<x;i++)
+        {
+            print("Michał");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
