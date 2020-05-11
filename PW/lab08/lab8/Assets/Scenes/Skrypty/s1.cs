69955dc8f986eb26cced1a9c3fcef49429b558b4
Trefluuu <miulewski@gmail.com>
2020-05-12 01:55:48 +0200

    lab08

---
+++ PW/lab08/lab8/Assets/Scenes/Skrypty/s1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class s1 : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] szukane = GameObject.FindGameObjectsWithTag("kula");
+        foreach (GameObject szukany in szukane)
+            GameObject.Destroy(szukany);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
