415a7262db36448363bb2e6b545b907905e12706
Trefluuu <miulewski@gmail.com>
2020-05-12 00:01:26 +0200

    lab06

---
+++ PIPO2/lab06/lab6_1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lab6_1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Silnia_0()
+        {
+            silnia S0 = new silnia();
+            S0.n = 0;
+            Assert.AreEqual(S0.oblicz(), 1);
+        }
+        [TestMethod]
+        public void Silnia_1()
+        {
+            silnia S1 = new silnia();
+            S1.n = 1;
+            Assert.AreEqual(S1.oblicz(), 1);
+        }
+        [TestMethod]
+        public void Silnia_5()
+        {
+            silnia S5 = new silnia();
+            S5.n = 5;
+            Assert.AreEqual(S5.oblicz(), 120);
+        }
+    }
+}
---
+++ PIPO2/lab06/lab6_1/lab6_1/silnia.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6_1
+{
+    public class silnia
+    {
+        public int n;
+        public int oblicz()
+        {
+            int wynik = 1;
+            if (n < 1)
+                return 1;
+            else 
+            { 
+            while (n > 0)
+            {
+                wynik *= n;
+                n--;
+            }
+            return wynik;
+            }
+        }
+    }
+}
