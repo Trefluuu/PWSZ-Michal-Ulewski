f898282b6c80779b713134d6b428e7f12cf3a20b
Trefluuu <miulewski@gmail.com>
2020-06-02 22:39:13 +0200

    Lab10

---
+++ PW/Lab10/Assets/Kulka.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kulka : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    public GameObject kuleczka;
+    public Material[] kolory;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal")* Time.deltaTime * speed;
+        //print(x);
+        float y = Input.GetAxis("Vertical")*Time.deltaTime*speed;
+        //print(y);
+        transform.Translate(x, y, 0);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            int kolor = Random.Range(0, kolory.Length);
+            kuleczka.transform.GetComponent<Renderer>().material = kolory[kolor];
+            //print(Input.mousePosition.x + "   " + Input.mousePosition.y);
+            Vector3 pozycja = Input.mousePosition;
+            pozycja.z = 15f;
+            pozycja = Camera.main.ScreenToWorldPoint(pozycja);
+            Instantiate(kuleczka, pozycja, Quaternion.identity);
+        }
+    }
+}
